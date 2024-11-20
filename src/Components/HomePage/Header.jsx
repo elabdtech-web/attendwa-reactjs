60708221024a5da31842f816a358b27c85cd25ca
Numanafsar <nomanafsar46@gmail.com>
2024-11-20 15:00:33 +0500

    Enterance is updated

--- src/Components/HomePage/Header.jsx
+++ src/Components/HomePage/Header.jsx
@@ -1,18 +1,28 @@
+import React, { useState, useEffect } from "react";
+import {auth} from "../../Firebase/FirebaseConfig"
 import Button from "./Button";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function Header() {
+  const [accessToken, setAccessToken] = useState(null);
+  const token = auth.currentUser.uid;
   return (
     <div className="flex justify-between items-center px-[5%] bg-gray-600 h-[10vh]">
       <div className="flex items-center gap-3">
-        <img src="/logo.png" alt="" className="size-14"/>
+        <img src="/logo.png" alt="" className="size-14" />
         <h1 className="font-semibold text-[20px] text-white">ELABD TECH</h1>
       </div>
       <div>
-        <Link to="/login">
-        <Button text={"Login"}/>
-        </Link>
+        {token  ? (
+          <Link to="/dashboard">
+            <Button text={"Dashboard"} />
+          </Link>
+        ) : (
+          <Link to="/login">
+            <Button text={"Login"} />
+          </Link>
+        )}
       </div>
     </div>
-  )
+  );
 }
