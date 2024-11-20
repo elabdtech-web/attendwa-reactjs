dd97ede5ccf404df5effe79e9daeaa0fdca557da
Numanafsar <nomanafsar46@gmail.com>
2024-11-20 15:21:59 +0500

    Error Resolved in hompage

--- src/Components/HomePage/Header.jsx
+++ src/Components/HomePage/Header.jsx
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from "react";
-import {auth} from "../../Firebase/FirebaseConfig"
+import { auth } from "../../Firebase/FirebaseConfig";
 import Button from "./Button";
 import { Link } from "react-router-dom";
 
 export default function Header() {
   const [accessToken, setAccessToken] = useState(null);
-    if (auth.currentUser) {
-      setAccessToken(auth.currentUser.uid);
-    } else {
-      setAccessToken(null);
-    }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setAccessToken(user.uid);  // Set accessToken to user's UID when user is authenticated
+      } else {
+        setAccessToken(null);  // Set accessToken to null when there is no user
+      }
+    });
+
+    // Cleanup the listener on component unmount
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="flex justify-between items-center px-[5%] bg-gray-600 h-[10vh]">
       <div className="flex items-center gap-3">
@@ -17,7 +26,7 @@ export default function Header() {
         <h1 className="font-semibold text-[20px] text-white">ELABD TECH</h1>
       </div>
       <div>
-        {accessToken  ? (
+        {accessToken ? (
           <Link to="/dashboard">
             <Button text={"Dashboard"} />
           </Link>
